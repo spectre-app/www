0a93d98a93f50f7b32c99e2534fdbf4ddbc0ef7e
Maarten Billemont <lhunath@lyndir.com>
2021-01-15 10:12:23 -0500

    Improve quality of resources and effects.

--- themes/2020/assets/js/script.js
+++ themes/2020/assets/js/script.js
@@ -1,83 +1,56 @@
-/* ========================================================================= */
-/*	Page Preloader
-/* ========================================================================= */
-
-$(window).on('load', function () {
-	$('.preloader').fadeOut(100);
-});
-
 jQuery(function ($) {
-	"use strict";
+    "use strict";
 
+    /*	Close navbar when link clicked. */
+    $('.navbar-collapse a').click(function () {
+        $(".navbar-collapse").collapse('hide');
+    });
 
-	/* ========================================================================= */
-	/*	Auto close Navbar when click on link
-	/* ========================================================================= */
+    /* Lazy load images. */
+    lozad().observe();
 
-	$('.navbar-collapse a').click(function () {
-		$(".navbar-collapse").collapse('hide');
-	});
+    /* Pop-up image zoom. */
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 160, //delay removal by X to allow out-animation
+        closeOnContentClick: true,
+        midClick: true,
+        fixedContentPos: false,
+        fixedBgPos: true
+    });
 
+    /*	Animated page scroll. */
+    var html_body = $('html, body');
+    $('nav a, .page-scroll').on('click', function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 50
+                }, 1500, 'easeInOutExpo');
+                return false;
+            }
+        }
+    });
 
-	/* ========================================================================= */
-	/*	lazy load initialize
-	/* ========================================================================= */
+    /*	easeInOutExpo animator. */
+    jQuery.extend(jQuery.easing, {
+        easeInOutExpo: function (x, t, b, c, d) {
+            if (t === 0) {
+                return b;
+            }
+            if (t === d) {
+                return b + c;
+            }
+            if ((t /= d / 2) < 1) {
+                return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            }
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    });
 
-	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
-	observer.observe();
+    /*	OS detection. */
+    $('html').attr('data-os', UAParser().os.name);
 
-	/* ========================================================================= */
-	/*	Magnific popup
-	/* =========================================================================  */
-	$('.image-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: false,
-		fixedBgPos: true
-	});
-
-	/* ========================================================================= */
-	/*	animation scroll js
-	/* ========================================================================= */
-
-	var html_body = $('html, body');
-	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				html_body.animate({
-					scrollTop: target.offset().top - 50
-				}, 1500, 'easeInOutExpo');
-				return false;
-			}
-		}
-	});
-
-	// easeInOutExpo Declaration
-	jQuery.extend(jQuery.easing, {
-		easeInOutExpo: function (x, t, b, c, d) {
-			if (t === 0) {
-				return b;
-			}
-			if (t === d) {
-				return b + c;
-			}
-			if ((t /= d / 2) < 1) {
-				return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-			}
-			return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-		}
-	});
-
-        $('html').attr('data-os', UAParser().os.name);
 });
